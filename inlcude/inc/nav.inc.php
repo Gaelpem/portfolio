4facdb011691968be501010bb2406c7f7476d7e1
Gaelpem <pembelegael@gmail.com>
2024-12-05 12:39:48 +0100

    link-anim

--- inlcude/inc/nav.inc.php
+++ inlcude/inc/nav.inc.php
@@ -2,12 +2,12 @@
             <div class="residence">
                 <h3>Paris, France</h3>
             </div>
-            <div class="links">
-                <ul>
-                    <li><a href="Home">Home .</a></li>
-                    <li><a href="Projets">Projects .</a></li>
-                    <li><a href="About">About .</a></li>
-                    <li><a href="Contact">Contact .</a></li>
+            <div class="container-links">
+                <ul class="links">
+                    <li><a href="Home">Home,</a></li>
+                    <li><a href="Projets">Projects,</a></li>
+                    <li><a href="About">About,</a></li>
+                    <li><a href="Contact">Contact,</a></li>
                 </ul>
             </div>
         </div>
