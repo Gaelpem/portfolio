e4095c593d95fcc5a4d5ba149d79a0ae3da5bba3
Gaelpem <pembelegael@gmail.com>
2024-12-07 00:01:14 +0100

    contact2

--- inlcude/contact.php
+++ inlcude/contact.php
@@ -1,7 +1,12 @@
 <div id="contact-menu">
     <h2 class="titre-contact">[Conta-ct]</h2>
      <form action="">
-
+      <label for="nom">Name:</label>
+      <input type="text" name="name" id="nom">
+      <label for="surname">Surname:</label>
+      <input type="text" name="prenom" id="surname">
+      <label for="mail">Email:</label>
+      <input type="email" name="email" id="mail">
      </form>
 
 </div>
