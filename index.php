e9f9244b962b8050df4821852015d3aaf4a92b63
Gaelpem <pembelegael@gmail.com>
2024-12-19 23:47:59 +0100

    miseAjour

--- index.php
+++ index.php
@@ -10,24 +10,25 @@
          <div class="trait-horizontal1"></div>
         <main>
           <div class="logo">
-            <img src="asset/img/logo.png" alt="">
+            <img src="asset/img/logo.svg" alt="">
           </div>
                <div class="form1"></div>
                <div class="form2"></div>
            <div class="para">
-              <h2><span class="salut">Salut!</span>
+              <h2><span class="salut">Salut__!</span>
               <span class="para_mini">Je m'appelle Gaël Pembele, je suis un apprenti développeur, passioné par la technologie et le design, c'est pourquoi j'ai choisi de combiner ces deux univers ensemble.</span></h2>
           </div>  
+
           <div id="projet-menu">
-               <h2 class="titre-projet">[Pro-jects]<span class="petit-numero">(001)<span></h2>
-           
+               <h2 class="titre-projet">[Pro-jects]<span class="petit-numero1">(001)<span></h2>
+               
+               <div class="trait-horizontal2"></div>
           </div>
-          <hr/>
+         
           <div id="about-menu">
           <div class="form3"></div>
                <div class="form4"></div>
-               <h2 class="titre-about">[Ab-out]<span class="petit-numero">(002)<span></h2>
-             
+               <h2 class="titre-about">[Ab-out]<span class="petit-numero2">(002)<span></h2>
                     <p class="mini_para1">
                     1/ Tant qu’étudiant en première année de développement web àDigital Campus Paris. Passionné par lefront-end et le design, j’apprécie l’aspect créatif du développement web. Souhaitant devenir un développeur complet, j’explore également le back-end.
                     </p>
@@ -37,7 +38,7 @@
                    </p>
                
           </div>
-          <hr/>
+     
           <?php require "asset/inc/contact.php"; ?>
      </main >
         
